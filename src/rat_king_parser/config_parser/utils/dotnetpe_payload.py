b66fb7a287e1b99cf0e7947d2c1a6e4513e3673e
doomedraven <abrukhovetskyy@google.com>
2024-12-08 11:43:02 +0100

    Update dotnetpe_payload.py

--- src/rat_king_parser/config_parser/utils/dotnetpe_payload.py
+++ src/rat_king_parser/config_parser/utils/dotnetpe_payload.py
@@ -28,7 +28,6 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-import struct
 from dataclasses import dataclass
 from hashlib import sha256
 from logging import getLogger
